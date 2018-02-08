d232db816b51e59fa890d105f6fa54d33280a58d
Mark De May <markd@frii.com>
2018-02-08 16:18:35 -0700

    Action and reducer for adding a list of tasklists to ActiveBoard

--- src/Reducers/ActiveBoardReducer.js
+++ src/Reducers/ActiveBoardReducer.js
@@ -1,11 +1,15 @@
 import {
-  SET_ACTIVE_BOARD
+  SET_ACTIVE_BOARD,
+  ADD_ACTIVE_BOARD_TASKLISTS
 } from '../Actions/ActionTypes';
 
 export const ActiveBoard = (state = {}, action) => {
   switch (action.type) {
     case (SET_ACTIVE_BOARD):
       return action.board;
+    case (ADD_ACTIVE_BOARD_TASKLISTS):
+      const tasklists = action.tasklists;
+      return {...state, tasklists};
     default:
       return state;
   }
