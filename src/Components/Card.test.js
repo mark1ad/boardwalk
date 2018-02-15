683c21759dc1c0e50bb3cc868c46cc037dcd279c
Mark De May <markd@frii.com>
2018-02-15 09:45:53 -0700

    Card component: added unit test for click event

--- src/Components/Card.test.js
+++ src/Components/Card.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import renderer from 'react-test-renderer';
+import {shallow } from 'enzyme';
 import Card from './Card';
+
+const mockSubmitFunction = jest.fn();
 
 it('renders without crashing', () => {
   const id = 1;
@@ -17,4 +20,11 @@ it("renders correctly", () => {
     .create(<Card name="name" id={id} submit={submit} class="class" />)
     .toJSON();
   expect(tree).toMatchSnapshot();
+})
+
+it("should call the mock sumbit function", () => {
+  const id = 1;
+  const card = shallow(<Card name="name" id={id} submit={mockSubmitFunction} class="test-card" />);
+  card.simulate('click', {preventDefault() {}})
+  expect(mockSubmitFunction.mock.calls.length).toBe(1);
 })
